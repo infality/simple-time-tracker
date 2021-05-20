db6dec82af666c7db8972fee364944db8aabba2a
Infality <infality@420blaze.it>
2021-05-20 23:58:33 +0200

    Added SQLite to store and load tracked times

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use iced::{
     Clipboard, Color, Column, Command, Container, Element, Length, Row, Rule, Scrollable, Settings,
     Space, Subscription, Text, TextInput, Tooltip,
 };
+use rusqlite::{params, Connection};
 
 pub fn main() -> iced::Result {
     SimpleTimeTracker::run(Settings {
@@ -47,7 +48,7 @@ struct TrackedTime {
 }
 
 impl TrackedTime {
-    fn new(description: String, duration: chrono::Duration) -> Self {
+    fn new(duration: chrono::Duration, description: String) -> Self {
         TrackedTime {
             description,
             duration,
@@ -123,7 +124,7 @@ impl SimpleTimeTracker {
 
         if self.description_input.len() > 0 {
             self.tracked_times
-                .push(TrackedTime::new(self.description_input.clone(), duration));
+                .push(TrackedTime::new(duration, self.description_input.clone()));
         } else {
             let index = self.index_input.parse::<usize>().unwrap();
             if index == 0 || index > self.tracked_times.len() {
@@ -147,6 +148,22 @@ impl SimpleTimeTracker {
             self.pause_time = self.pause_time - duration;
         }
     }
+
+    fn write_to_db(&self) {
+        let db = Connection::open("simple_time_tracker.sqlite").unwrap();
+
+        db.execute("DELETE FROM TrackedTimes", []).unwrap();
+        let mut stmt = db
+            .prepare("INSERT INTO TrackedTimes (Seconds, Description) VALUES (?1, ?2)")
+            .unwrap();
+        for tracked_time in self.tracked_times.iter() {
+            stmt.execute(params![
+                tracked_time.duration.num_seconds(),
+                tracked_time.description
+            ])
+            .unwrap();
+        }
+    }
 }
 
 impl Application for SimpleTimeTracker {
@@ -155,13 +172,38 @@ impl Application for SimpleTimeTracker {
     type Flags = ();
 
     fn new(_flags: ()) -> (Self, Command<Message>) {
+        let db = Connection::open("simple_time_tracker.sqlite").unwrap();
+
+        db.execute(
+            "CREATE TABLE IF NOT EXISTS TrackedTimes (
+                ID	INTEGER PRIMARY KEY,
+                Seconds	INTEGER NOT NULL,
+                Description	TEXT NOT NULL
+        )",
+            [],
+        )
+        .unwrap();
+
+        let mut stmt = db
+            .prepare("SELECT Seconds, Description FROM TrackedTimes")
+            .unwrap();
+        let mut rows = stmt.query([]).unwrap();
+
+        let mut tracked_times = Vec::new();
+        while let Some(row) = rows.next().unwrap() {
+            tracked_times.push(TrackedTime::new(
+                chrono::Duration::seconds(row.get(0).unwrap()),
+                row.get(1).unwrap(),
+            ));
+        }
+
         (
             Self {
                 is_dark_mode: true,
                 is_running: false,
                 start_time: chrono::Local::now(),
                 pause_time: chrono::Local::now(),
-                tracked_times: Vec::new(),
+                tracked_times: tracked_times,
 
                 start_stop_button: button::State::new(),
                 dark_mode_button: button::State::new(),
@@ -215,9 +257,16 @@ impl Application for SimpleTimeTracker {
             }
             Message::ApplyOperation => {
                 self.apply_operation();
+                self.write_to_db();
             }
-            Message::DeleteTrackedTime(i) => drop(self.tracked_times.remove(i)),
-            Message::CopyText(i) => clipboard.write(self.tracked_times[i].description.clone()),
+            Message::DeleteTrackedTime(i) => {
+                self.tracked_times.remove(i);
+                self.write_to_db();
+            }
+            Message::CopyText(i) => {
+                clipboard.write(self.tracked_times[i].description.clone());
+                self.write_to_db();
+            }
         }
 
         Command::none()
@@ -584,17 +633,18 @@ impl Application for SimpleTimeTracker {
 }
 
 const _SUN_SVG: &'static str = "<svg viewBox=\"0 0 457.32 457.32\" height=\"10\">
-	<path d=\"M228.66,112.692c-63.945,0-115.968,52.022-115.968,115.967c0,63.945,52.023,115.968,115.968,115.968
-		s115.968-52.023,115.968-115.968C344.628,164.715,292.605,112.692,228.66,112.692z\"/>
-	<path d=\"M401.429,228.66l42.467-57.07c2.903-3.9,3.734-8.966,2.232-13.59c-1.503-4.624-5.153-8.233-9.794-9.683
-		l-67.901-21.209l0.811-71.132c0.056-4.862-2.249-9.449-6.182-12.307c-3.934-2.858-9.009-3.633-13.615-2.077l-67.399,22.753
-		L240.895,6.322C238.082,2.356,233.522,0,228.66,0c-4.862,0-9.422,2.356-12.235,6.322l-41.154,58.024l-67.4-22.753
-		c-4.607-1.555-9.682-0.781-13.615,2.077c-3.933,2.858-6.238,7.445-6.182,12.307l0.812,71.132l-67.901,21.209
-		c-4.641,1.45-8.291,5.059-9.793,9.683c-1.503,4.624-0.671,9.689,2.232,13.59l42.467,57.07l-42.467,57.07
-		c-2.903,3.9-3.734,8.966-2.232,13.59c1.502,4.624,5.153,8.233,9.793,9.683l67.901,21.208l-0.812,71.132
-		c-0.056,4.862,2.249,9.449,6.182,12.307c3.934,2.857,9.007,3.632,13.615,2.077l67.4-22.753l41.154,58.024
-		c2.813,3.966,7.373,6.322,12.235,6.322c4.862,0,9.422-2.356,12.235-6.322l41.154-58.024l67.399,22.753
-		c4.606,1.555,9.681,0.781,13.615-2.077c3.933-2.858,6.238-7.445,6.182-12.306l-0.811-71.133l67.901-21.208
-		c4.641-1.45,8.291-5.059,9.794-9.683c1.502-4.624,0.671-9.689-2.232-13.59L401.429,228.66z M228.66,374.627
-		c-80.487,0-145.968-65.481-145.968-145.968S148.173,82.692,228.66,82.692s145.968,65.48,145.968,145.967
-		S309.147,374.627,228.66,374.627z\"/></svg>";
+        <path d=\"M228.66,112.692c-63.945,0-115.968,52.022-115.968,115.967c0,63.945,52.023,115.968,115.968,115.968
+            s115.968-52.023,115.968-115.968C344.628,164.715,292.605,112.692,228.66,112.692z\"/>
+        <path d=\"M401.429,228.66l42.467-57.07c2.903-3.9,3.734-8.966,2.232-13.59c-1.503-4.624-5.153-8.233-9.794-9.683
+            l-67.901-21.209l0.811-71.132c0.056-4.862-2.249-9.449-6.182-12.307c-3.934-2.858-9.009-3.633-13.615-2.077l-67.399,22.753
+            L240.895,6.322C238.082,2.356,233.522,0,228.66,0c-4.862,0-9.422,2.356-12.235,6.322l-41.154,58.024l-67.4-22.753
+            c-4.607-1.555-9.682-0.781-13.615,2.077c-3.933,2.858-6.238,7.445-6.182,12.307l0.812,71.132l-67.901,21.209
+            c-4.641,1.45-8.291,5.059-9.793,9.683c-1.503,4.624-0.671,9.689,2.232,13.59l42.467,57.07l-42.467,57.07
+            c-2.903,3.9-3.734,8.966-2.232,13.59c1.502,4.624,5.153,8.233,9.793,9.683l67.901,21.208l-0.812,71.132
+            c-0.056,4.862,2.249,9.449,6.182,12.307c3.934,2.857,9.007,3.632,13.615,2.077l67.4-22.753l41.154,58.024
+            c2.813,3.966,7.373,6.322,12.235,6.322c4.862,0,9.422-2.356,12.235-6.322l41.154-58.024l67.399,22.753
+            c4.606,1.555,9.681,0.781,13.615-2.077c3.933-2.858,6.238-7.445,6.182-12.306l-0.811-71.133l67.901-21.208
+            c4.641-1.45,8.291-5.059,9.794-9.683c1.502-4.624,0.671-9.689-2.232-13.59L401.429,228.66z M228.66,374.627
+            c-80.487,0-145.968-65.481-145.968-145.968S148.173,82.692,228.66,82.692s145.968,65.48,145.968,145.967
+            S309.147,374.627,228.66,374.627z\"/>
+    </svg>";
